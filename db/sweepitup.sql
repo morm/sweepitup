841358beaabe817d7c93766c479518789695e5b0
const <vahkos@mail.ru>
2014-07-25 18:53:27 +0300

    db changed

--- db/sweepitup.sql
+++ db/sweepitup.sql
@@ -1,6 +1,6 @@
 /*
 SQLyog Professional v11.51 (64 bit)
-MySQL - 5.6.14-log : Database - seepitup
+MySQL - 5.6.14-log : Database - sweepitup
 *********************************************************************
 */
 
@@ -12,16 +12,34 @@ MySQL - 5.6.14-log : Database - seepitup
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*Table structure for table `average_garbage` */
+
+CREATE TABLE `average_garbage` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `date` datetime NOT NULL,
+  `glass` int(11) unsigned zerofill DEFAULT NULL,
+  `plastmass` int(11) unsigned zerofill DEFAULT NULL,
+  `paper` int(11) unsigned zerofill DEFAULT NULL,
+  `metall` int(11) unsigned zerofill DEFAULT NULL,
+  `stones` int(11) unsigned zerofill DEFAULT NULL,
+  `sorted` tinyint(1) DEFAULT NULL,
+  `longitude` char(1) NOT NULL,
+  `latitude` char(1) NOT NULL,
+  `username` char(1) DEFAULT NULL,
+  `email` char(1) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 /*Table structure for table `garbage` */
 
 CREATE TABLE `garbage` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `date` datetime NOT NULL,
-  `glass` int(11) DEFAULT NULL,
-  `plastmass` int(11) DEFAULT NULL,
-  `paper` int(11) DEFAULT NULL,
-  `metall` int(11) DEFAULT NULL,
-  `stones` int(11) DEFAULT NULL,
+  `glass` int(11) unsigned zerofill DEFAULT NULL,
+  `plastmass` int(11) unsigned zerofill DEFAULT NULL,
+  `paper` int(11) unsigned zerofill DEFAULT NULL,
+  `metall` int(11) unsigned zerofill DEFAULT NULL,
+  `stones` int(11) unsigned zerofill DEFAULT NULL,
   `sorted` tinyint(1) DEFAULT NULL,
   `longitude` char(1) NOT NULL,
   `latitude` char(1) NOT NULL,
